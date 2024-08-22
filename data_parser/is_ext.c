7cb8158f5548fcb0d965ed67236a67f1107565d7
nerdchanii <nerdchanii@gmail.com>
2024-08-22 10:39:05 +0900

    feat(parser): blue print of parser

--- data_parser/is_ext.c
+++ data_parser/is_ext.c
@@ -6,17 +6,21 @@
 /*   By: yechakim <yechakim@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/05 17:01:28 by yechakim          #+#    #+#             */
-/*   Updated: 2024/07/15 21:13:00 by yechakim         ###   ########.fr       */
+/*   Updated: 2024/08/20 10:38:03 by yechakim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d_parser.h"
+#include <stdio.h>
 
 int	is_ext(char *file, char *ext)
 {
 	char	*file_ext;
 
 	if (!file || !ext)
+		return (0);
+	file_ext = ft_strrchr(file, '.');
+	if (!file_ext)
 		return (0);
 	return (ft_strncmp(file_ext, ext, ft_strlen(ext) + 1) == 0);
 }
